24eaa5279c68e674451c7b5208d6cd39efc98c60
Steesha <2327488733@qq.com>
2020-07-24 12:02:54 +0800

    添加启动命令行~

--- PictureObfuscator.cpp
+++ PictureObfuscator.cpp
@@ -7,7 +7,7 @@
 //  scope and manner of such use.
 //==============================================================
 //  Create by Steesha at 2020.7.24.
-//  Version 1.1
+//  Version 1.2
 //  Steesha [steesha@qq.com]
 //  注：不建议使用管理员模式启动，否则将无法向控制台里面拖入文件
 //  用法：详见README.md
@@ -48,9 +48,41 @@ string getOutputName(string inputName, bool IsDeobfuscate);// e.g:  C:\MyFile.pn
 void setConsoleColor(WORD Attri);
 //Method
 
-int main()
+int main(int argc, char* argv[])
 {
-	
+	//获取启动命令行
+	bool commandstart = false;
+	string arg_input;
+	int arg_method = -1, arg_seed = -1;
+	for (int i = 0; i < argc; i++) {
+		if (i<= (argc - 1)) {
+			string str = argv[i];//必须得转换一下string
+			if (str == "--input") {
+				arg_input = argv[i + 1];
+				commandstart = true;
+			}
+			else if (str == "--method") {
+				arg_method = *argv[i + 1] - '0';
+				commandstart = true;
+			}
+			else if (str == "--seed") {
+				arg_seed = *argv[i + 1] - '0';
+				commandstart = true;
+			}
+		}
+	}
+	if (arg_input != "" && arg_method != -1 && arg_seed != -1 && commandstart) {//Start in command mode
+		setConsoleColor(greenColor);
+		cout << "------->>>Process start in command mode." << endl;
+		setConsoleColor(defaultColor);
+	}
+	else if(commandstart){
+		setConsoleColor(redColor);
+		cout << "COMMAND INVALID. Process Exit...." << endl;
+		cout << "Correct Command Format:" << endl << "--input <pictureFile> --method <1/0> --seed <seed>" << endl;
+		setConsoleColor(defaultColor);
+		return -1;
+	}
 	setConsoleColor(defaultColor);
 	system("title PictureObfuscator");
 	//->Start
@@ -59,14 +91,19 @@ int main()
 	cout << "PictureObfuscator";
 	setConsoleColor(defaultColor);
 	cout << "> By Steesha.[Release on 2020 Jul. 23rd]" << endl;
-	cout << ">> Input your Picture File. FileName:[e.g: C:\\MyFile.jpg]" << endl;
-	cout << ">>If your inputfile is:[e.g: C:\\MyFile.jpg], outputfile will be:[e.g: C:\\MyFile_(de)obfuscated.jpg]" << endl;
 	//<-Start
-
-	//->Input
 	string inputFileName;
-	cin >> inputFileName;
-	//<-Input
+	
+	if (!commandstart) {//Command Mode.
+		cout << ">> Input your Picture File. FileName:[e.g: C:\\MyFile.jpg]" << endl;
+		cout << ">>If your inputfile is:[e.g: C:\\MyFile.jpg], outputfile will be:[e.g: C:\\MyFile_(de)obfuscated.jpg]" << endl;
+		//->Input
+		cin >> inputFileName;
+		//<-Input
+	}
+	else {
+		inputFileName = arg_input;
+	}
 
 	//->init
 	GDIStartupInfomation GdiInfo = StartupGDI();//初始化GDI
@@ -86,16 +123,29 @@ int main()
 
 	//->Choose Method
 	bool IsDeobfuscate;//是否进行反混淆操作 true->Deobf    false->Obf
-	cout << "METHOD: " << endl << "[0]Obfuscate Picture" << endl << "[1]Deobfuscate Picture" << endl;
-	cin >> IsDeobfuscate;
-	cout << "YOU chose <" << (IsDeobfuscate ? "Deobfuscate" : "Obfuscate") << "Picture" << ">" << endl;
+	if (!commandstart) {//Command Mode.
+		cout << "METHOD: " << endl << "[0]Obfuscate Picture" << endl << "[1]Deobfuscate Picture" << endl;
+		cin >> IsDeobfuscate;
+		cout << "YOU chose <" << (IsDeobfuscate ? "Deobfuscate" : "Obfuscate") << "Picture" << ">" << endl;
+		
+		cout << "---------------------------------------------------------------------------------------------------" << endl;
+	}
+	else {
+		IsDeobfuscate = arg_method;
+	}
 	//<-Choose Method
-	cout << "---------------------------------------------------------------------------------------------------" << endl;
+
 	//->Enter Seed
-	cout << "Enter Your  obf/deobf seed[int]:";
 	int pseed;
-	cin >> pseed;
-	//<-Enter Seed
+	if (!commandstart) {//Command Mode.
+		cout << "Enter Your  obf/deobf seed[int]:";
+		cin >> pseed;
+		//<-Enter Seed
+	}
+	else {
+		pseed = arg_seed;
+	}
+
 	cout << "---------------------------------------------------------------------------------------------------" << endl;
 	cout << "Processing......." << endl;
 	system(IsDeobfuscate ? "title Deobfuscating" : "title Obfuscating");
@@ -117,10 +167,12 @@ int main()
 		cout << "SAVE PICTURE Failed, ERRORCODE <" << save_ret << ">" << endl;
 		setConsoleColor(defaultColor);
 		cout << "Process Exit....." << endl;
-		ShutdownGDI(GdiInfo);//关闭GDI
 		delete picture;//DELETE
-		system("pause");
-		return -1;
+		ShutdownGDI(GdiInfo);//关闭GDI
+		if (!commandstart) {//Command Mode.
+			system("pause");
+		}
+		return save_ret;
 	}
 	else {
 		cout << "SAVE PICTURE ";
@@ -140,7 +192,9 @@ int main()
 	//->Shutdown
 	delete picture;//DELETE
 	ShutdownGDI(GdiInfo);//关闭GDI
-	system("pause");
+	if (!commandstart) {//Command Mode.
+		system("pause");
+	}
 	return 0;
 	//<-Shutdown
 }
@@ -224,7 +278,7 @@ int GetSeeds(int Rseed, vector<int>& TxSeed, vector<int>& TySeed, Bitmap* pictur
 		for (UINT j = 0; j < picture->GetWidth(); j++) {
 			TxSeed.push_back(getRand(0, picture->GetWidth()));
 			start_t = clock();
-			if (start_t % 100 == 0) {
+			if (start_t % 10 == 0) {
 				cout << "[Creating Seeds]" << ((float)lcounter / full * 100) / 2 << "%" << endl;
 			}
 			lcounter++;
@@ -251,7 +305,7 @@ Bitmap* Obfuscate(Bitmap* picture, int Rseed) {
 			picture->SetPixel(TxSeed[x], TySeed[y], color_old);
 			picture->SetPixel(x, y, color_new);
 			start_t = clock();
-			if (start_t % 100 == 0) {
+			if (start_t % 10 == 0) {
 				cout << "[Obfuscate]" << 50.0 + ((float)lcounter / full * 100) / 2 << "%" << endl;
 			}
 			lcounter++;
@@ -280,7 +334,7 @@ Bitmap* Deobfuscate(Bitmap* picture, int Rseed) {
 			picture->SetPixel(TxSeed[x], TySeed[y], color_old);
 			picture->SetPixel(x, y, color_new);
 			start_t = clock();
-			if (start_t % 100 == 0) {
+			if (start_t % 10 == 0) {
 				cout << "[Deobfuscate]" << 50.0 + ((float)lcounter / full * 100) / 2 << "%" << endl;
 			}
 			lcounter++;
